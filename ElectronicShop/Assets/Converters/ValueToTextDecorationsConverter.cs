793febc989e2f63ce26b76bbc188674d84bd38e9
user_3_19P1 <user_3_19p1@mail.ru>
2023-06-02 11:55:31 +0500

    x

--- ElectronicShop/Assets/Converters/ValueToTextDecorationsConverter.cs
+++ ElectronicShop/Assets/Converters/ValueToTextDecorationsConverter.cs
@@ -8,6 +8,12 @@
             if (str == null) { return default; }
             else if (str == 0) { return default; }
             else { return TextDecorations.Strikethrough; }
+            //var str = value as double?;
+            //string str1 = str.ToString();
+            //int len = str1.Length; 
+            //if (len <=3) { return default; }
+            //else if (str == 0) { return default; }
+            //else { return TextDecorations.Strikethrough; }
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
